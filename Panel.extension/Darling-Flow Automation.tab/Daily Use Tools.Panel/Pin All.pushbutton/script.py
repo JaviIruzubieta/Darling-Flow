60aaf066fb181522634ab6e29d0a1aaf8aeea481
Javier Iruzubieta <75833127+JaviIruzubieta@users.noreply.github.com>
2022-07-27 12:17:33 +0100

    Add files via upload

---
+++ Panel.extension/Darling-Flow Automation.tab/Daily Use Tools.Panel/Pin All.pushbutton/script.py
@@ -0,0 +1,76 @@
+# Load the Python Standard and DesignScript Libraries
+import sys
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+import clr
+import System
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('DSCoreNodes')
+from DSCore.List import Flatten
+
+ 
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+ 
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+clr.ImportExtensions(Revit.GeometryConversion)
+
+viewsToKeep = ["Splash Screen", "Project View", "System Browser"]
+sheets = []
+
+CollectorLevels = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements()
+CollectorGrids = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Grids).WhereElementIsNotElementType().ToElements()
+CollectorRVTlinks = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks
+).WhereElementIsNotElementType().ToElements()
+CollectorProjectBase = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ProjectBasePoint
+).WhereElementIsNotElementType().ToElements()
+CollectorImportInstances = FilteredElementCollector(doc).OfClass(ImportInstance
+).WhereElementIsNotElementType().ToElements()
+
+ElementsCollector = []
+
+ElementsCollector.append(CollectorLevels)
+ElementsCollector.append(CollectorGrids)
+ElementsCollector.append(CollectorRVTlinks)
+ElementsCollector.append(CollectorProjectBase)
+ElementsCollector.append(CollectorImportInstances)
+
+FlattenList = Flatten(ElementsCollector)
+
+OutList =[]
+
+Transaction1 = Transaction(doc,"Pin all")
+Transaction1.Start()
+
+for s in FlattenList:
+	Element.Pinned.SetValue(s,True)
+	OutList.append(s)
+
+#for v in Collector:
+#	if (v.Name in viewsToKeep):
+#		Element.Pinned.SetValue(v,True)
+			
+			
+			#v.Element.Pinned.Set
+#		sheets.append(v)
+			#sheets.Pinned(True)
+Transaction1.Commit()
+#sheets.Pinned(True)
+#for v in FilteredElementCollector(doc).OfClass(View):
+#	if (v.IsTemplate == False) and (v.Name in viewsToKeep):
+#			viewIds.append(v.Id)
+
+
+
+OUT = OutList
---
+++ Panel.extension/Darling-Flow Automation.tab/Eden Campus.Panel/Splash Screen RevisionTEMP.pushbutton/script.py
@@ -0,0 +1,111 @@
+import clr
+# Import Element wrapper extension methods
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+#doc = DocumentManager.Instance.CurrentDBDocument
+#uidoc=DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+#app = uiapp.Application
+
+clr.AddReference('RevitAPIUI')
+#import everything from the UI Namespace...
+from Autodesk.Revit.UI import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+from datetime import datetime
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+from pyrevit import revit, DB, UI
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app = __revit__.Application
+
+import rpw
+from rpw.utils.dotnet import clr, Process
+from rpw.utils.logger import logger
+from rpw.base import BaseObject
+
+RunIt = True
+uwdoc = doc
+sheetList = []
+splashscreen = []
+
+
+if RunIt:
+	try:
+		errorReport = None
+		
+		TransactionManager.Instance.EnsureInTransaction(uwdoc)
+		
+		RevDescription = 'Suitable for Coordination'
+		NewRevision = Revision.Create(doc)
+		Description = NewRevision.Description = RevDescription;
+		
+		revDate = NewRevision.RevisionDate = "2022-07-22"
+		
+		revId = NewRevision.GetGeneratingElementIds
+		rev = NewRevision.Id
+		
+		revisions = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Revisions).WhereElementIsNotElementType().ToElements()
+		
+		#if isinstance(revisions, list):
+		#	revs = []
+		#	for i in revisions:
+		#		revs.append(UnwrapElement(i))
+		#else:
+		#	revs = [UnwrapElement(revisions)]
+		
+		sheetscollector = FilteredElementCollector(uwdoc)
+		sheetsEl = sheetscollector.OfCategory(BuiltInCategory.OST_Sheets).ToElements()
+		
+		for splashsheet in sheetsEl:
+			if splashsheet.Name == "Splash Screen":
+				splashscreen.append(splashsheet)
+		
+		TransactionManager.Instance.TransactionTaskDone()
+		
+		for i in splashscreen:
+			revisionsOnSheet = i.GetAdditionalRevisionIds()
+			for r in revisions:
+				if r.Id not in revisionsOnSheet:
+					revisionsOnSheet.Add(r.Id)
+				else:
+					continue
+			TransactionManager.Instance.EnsureInTransaction(doc)
+			i.SetAdditionalRevisionIds(revisionsOnSheet)
+			TransactionManager.Instance.TransactionTaskDone()
+			
+	except:
+		# if error accurs anywhere in the process catch it
+		import traceback
+		errorReport = traceback.format_exc()
+else:
+	errorReport = TaskDialog.Show("Darling-Flow","Revision not added. Please ensure Revisions are set to Numeric and Splash Screen exists.")
+
+
+#Assign your output to the OUT variable
+if errorReport == None:
+	Message = 'Revision Added to Splash Screen', revDate,RevDescription
+	MessageString = Message.ToString()
+	OUT = TaskDialog.Show("Darling-Flow",MessageString)
+	
+	'Revision Added to Splash Screen', revDate, RevDescription
+else:
+	OUT = errorReport
